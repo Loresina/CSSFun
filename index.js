a2bb920ca02709377f8c030f7c108dd7bee4ce16
Loresina <loresina86@gmail.com>
2024-05-17 14:13:28 +0700

    add: slider swiper

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 import "./src/styles/scss/app.scss";
 import "./src/styles/normalize.min.css";
 
-console.log("Hello, World!");
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const swiper = new Swiper(".swiper", {
+  modules: [Navigation, Pagination],
+  direction: "horizontal",
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
